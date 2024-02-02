aeb93ccd81272543c8d0b2577efc00c7856f8d78
Silas Bue Handgaard <silasbueh@hotmail.com>
2024-02-02 09:47:09 +0100

    setup environment variables and access token for huggingface datasets access

--- data-parser.py
+++ data-parser.py
@@ -1,3 +1,11 @@
+import os
 from datasets import load_dataset
+from dotenv import load_dotenv
 
-dataset = load_dataset("DDSC/dk-hate")
+# Load environment variables from .env file
+load_dotenv()
+
+dataset = load_dataset(
+    "DDSC/dkhate", token=os.getenv('HUGGING_FACE_ACCESS_TOKEN'))
+
+print(dataset)
