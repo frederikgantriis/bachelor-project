12455221d5171f999b6ebea2ba98d7e6a9862f6a
Silas Bue Handgaard <silasbueh@hotmail.com>
2024-02-02 10:47:13 +0100

    add -r flag option to parser to force redownload of dataset

--- data-parser.py
+++ data-parser.py
@@ -1,14 +1,15 @@
 import os
+import sys
 from datasets import load_dataset, load_from_disk
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
-if not os.path.isdir("./data"):
+# Retrieve data from huggingface and store in ./data folder only if ./data
+# folder doesn't exist or -r flag was used
+if not os.path.isdir("./data") or len(sys.argv) > 1 and sys.argv[1] == "-r":
     load_dataset(
         "DDSC/dkhate", token=os.getenv('HUGGING_FACE_ACCESS_TOKEN')).save_to_disk("./data")
 
 dataset = load_from_disk("./data")
-
-print(dataset)
