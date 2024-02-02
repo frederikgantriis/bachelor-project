203760aa2b783d2f65e9bfc5de0b5d0ea3d256d4
Silas Bue Handgaard <silasbueh@hotmail.com>
2024-02-02 10:31:20 +0100

    store dataset on disk to speed up reading

--- data-parser.py
+++ data-parser.py
@@ -1,11 +1,14 @@
 import os
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
-dataset = load_dataset(
-    "DDSC/dkhate", token=os.getenv('HUGGING_FACE_ACCESS_TOKEN'))
+if not os.path.isdir("./data"):
+    load_dataset(
+        "DDSC/dkhate", token=os.getenv('HUGGING_FACE_ACCESS_TOKEN')).save_to_disk("./data")
+
+dataset = load_from_disk("./data")
 
 print(dataset)
