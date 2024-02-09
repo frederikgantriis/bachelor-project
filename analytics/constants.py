16f1f3f2e787f9996a8ca96f8c195e67bbf213b5
Frederik Gantriis Møller <frmo24042@gmail.com>
2024-02-09 10:39:50 +0100

    Add f1-score and refactor analytics

---
+++ analytics/constants.py
@@ -0,0 +1,2 @@
+NOT = "NOT"
+OFF = "OFF"
--- analytics/test_analytics.py
+++ analytics/test_analytics.py
@@ -1,4 +1,5 @@
-from analytics import calculate_false_negatives, calculate_false_positives, calculate_true_negatives, calculate_true_positives, calculate_precision, calculate_recall
+from analytics.analyse import Analytics
+from constants import NOT, OFF
 
 
 """
@@ -8,27 +9,38 @@ True Negative: 1
 False Positive: 2
 False Negative: 1
 """
-test_result = ["NOT", "OFF", "NOT", "OFF", "OFF", "OFF"]
-result = ["NOT", "OFF", "OFF", "NOT", "NOT", "OFF"]
+test_result = [NOT, OFF, NOT, OFF, OFF, OFF]
+
+test_dataset = {}
+
+test_dataset["labels"] = [NOT, OFF, OFF, NOT, OFF, NOT]
+
+test_analytics = Analytics(test_result, test_dataset)
 
 
 def test_calculate_true_positives():
-    assert calculate_true_positives(test_result, result) == 2
+    assert test_analytics.calculate_true_positives() == 2
 
 
 def test_calculate_true_negative():
-    assert calculate_true_negatives(test_result, result) == 1
+    assert test_analytics.calculate_true_negatives() == 1
 
 
 def test_calculate_false_positives():
-    assert calculate_false_positives(test_result, result) == 2
+    assert test_analytics.calculate_false_positives() == 2
 
 
 def test_calculate_false_negatives():
-    assert calculate_false_negatives(test_result, result) == 1
+    assert test_analytics.calculate_false_negatives() == 1
+
 
 def test_calculate_precision():
-    assert calculate_precision(test_result, result) == 2 / 3
+    assert test_analytics.calculate_precision() == 2 / 3
+
 
 def test_calculate_recall():
-    assert calculate_recall(test_result, result) == 2 / 3
+    assert test_analytics.calculate_recall() == 2 / 3
+
+
+def test_f1_score():
+    assert test_analytics.f1_score() == 4 / 7
