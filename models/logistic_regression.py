f2d3b3b18aecc6672524240ec5506cf15333037e
Frederik Gantriis Møller <frmo24042@gmail.com>
2024-02-29 11:53:23 +0100

    fix: change return-type of is-hateful from bool to int

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -13,8 +13,8 @@ class LogisticRegression(MLAlgorithm):
     def sigmoid(self, x: float):
         return 1 / (1 + math.e ** (-x))
     
-    def is_hateful(self, word: str):
-        return word.lower() in self.hateful_words
+    def is_hateful(self, word: str) -> int:
+        return int(word.lower() in self.hateful_words)
 
     def crossentropy_loss(self, guess, expected):
         return -(expected * math.log(guess) + (1-expected) * math.log(1-guess))
