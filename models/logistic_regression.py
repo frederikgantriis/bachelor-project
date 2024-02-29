7049838c4b5605abe7eacd51b1dee27f2e968952
Frederik Gantriis Møller <frmo24042@gmail.com>
2024-02-29 10:55:06 +0100

    feat: add words from hurtlex as set and method is_hateful

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,12 +1,17 @@
 import math
+import pandas as pd
 
 from datasets import DatasetDict, Dataset
 from models.ml_algorithm import MLAlgorithm
 
+
 class LogisticRegression(MLAlgorithm):
     def __init__(self, dataset: DatasetDict) -> None:
         super().__init__(dataset)
-        
+        self.hateful_words: set = set(pd.read_csv("./hurtlex_DA.tsv", sep="\t")["lemma"]) 
 
-    def sigmoid(self, x:float):
-        return 1/(1+math.e**(-x))
+    def sigmoid(self, x: float):
+        return 1 / (1 + math.e ** (-x))
+    
+    def is_hateful(self, word: str):
+        return word.lower() in self.hateful_words
