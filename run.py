5606bd7d6daf8f13387aa7e83f43df70043506ca
kbej <kbej@itu.dk>
2024-03-14 11:59:01 +0100

    Logistic refactor with neew datasets

--- run.py
+++ run.py
@@ -1,18 +1,14 @@
 from data_parser import Datasets
+from models.logistic_regression import LogisticRegression
 from models.naive_bayes import NaiveBayes
 
 if __name__ == "__main__":
     dataset_train = Datasets("train")
     dataset_train.remove_dots()
-    print(dataset_train.to_dict()["OFF"][0])
-    dataset_train.remove_stop_words()
-    print(dataset_train.to_dict()["OFF"][0])
-
-    dataset_train.remove_stop_words().remove_dots()
-    print(dataset_train.to_dict()["OFF"][0])
-
-    nb1 = NaiveBayes(dataset_train)
+    
     dataset_test = Datasets("test")
     dataset_test.remove_dots()
+    nb1 = NaiveBayes(dataset_train)
     nb1.test(dataset_test.to_list())
-    
+    lr = LogisticRegression(dataset_train)
+    lr.test(dataset_test.to_list())
