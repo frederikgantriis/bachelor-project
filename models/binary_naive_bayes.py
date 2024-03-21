3737e43b0899cacab8d9a3e389e6f893dee71034
kbej <kbej@itu.dk>
2024-03-21 12:33:33 +0100

    Updated Binary_bayes to new data struct

--- models/binary_naive_bayes.py
+++ models/binary_naive_bayes.py
@@ -1,33 +1,60 @@
 
-from models.naive_bayes import NaiveBayes
-from sanitizer import Sanitizer
+from data_parser import Dataset
+from models.naive_bayes import NaiveBayes, count_words
+from spacy.tokens import Token, Doc
+from data_storage import TrainData
+import math
 import utils
 
 
 class Binary_naive_bayes(NaiveBayes):
-    # Changed from naive, only count uniqe words in each sentence
-    def extract_words_from_label(dataset, label):
-        extracted_words = {}
-        sentences = utils.extract_sentences_from_label(dataset, label)
+    def __init__(self, dataset: Dataset) -> None:
+        super().__init__(dataset)
+        self.train_data = TrainData("binary-naive-bayes")
 
-        for s in sentences:
-            s = Sanitizer(s).sanitize_simple()
-            for word in s:
-                if word not in extracted_words:
-                    extracted_words[word] = 1
-        return extracted_words
+    def train(self):  # pragma: no cover
+        for c in self.classes:  # type: ignore
+            # amount of instances with this class
+            n_classes = len(self.dataset)
+            # it gives a base chance for it being NOT or OFF based on the split in the dataset
+            self.logprior[c] = math.log10(n_classes / self.n_instances)
+            
+            #Changed from base: Only counts words ones pr comment     
+            words_in_class = utils.extract_unique_words_from_comments(self.dataset[c]) 
+            
+            n_words = count_words(words_in_class, self.vocabulary)
 
-    def find_class(self, test_instance: str, classes: list):  # pragma: no cover
+            for word in self.vocabulary:
+                count = words_in_class[word] if word in words_in_class else 0
+
+                # compute the likelihood of this word being generated from this class based on
+                # the amount of the word used in the class compared to the total amount of
+                # words used in the class.
+                self.loglikelihood[(word.text, c)] = math.log10(
+                    (count + 1) / (n_words - count)
+                )
+
+            # update the train data parameters
+            self.train_data.parameters = self.logprior, self.loglikelihood, self.classes
+            # save the train data to disk
+            self.train_data.save_to_disk()
+
+    def find_class(
+    comment: Doc, classes: list, logprior: dict, loglikelihood: dict
+):  # pragma: no cover
         sum = {}
-        test_instance_list = Sanitizer(test_instance).sanitize_simple()
         for c in classes:
+            sum[c] = logprior[c]
             seen_words = []
-            sum[c] = self.logprior[c]
-            for word in test_instance_list:
-                if word not in seen_words:  # Changed from naive, only counts uniqe words from comment
+            for word in comment:
+                #Changed from base: Only counts words ones pr comment   
+                if word not in seen_words:
                     seen_words.append(word)
                     try:
-                        sum[c] += self.loglikelihood[(word, c)]
+                        sum[c] += loglikelihood[(word.text, c)]
                     except KeyError:
                         continue
         return utils.get_max_value_key(sum)
+    
+    def __str__(self) -> str:
+        return "binary-naive-bayes"
