b455211cf1b5eeaa08c5cfc2f8862929144c1112
Frederik Gantriis Møller <frmo24042@gmail.com>
2024-04-10 19:34:49 +0200

    refactor: get  get_train_datasets and get_test_datasets functions in data_builder.py to use get_all_dataset_combinations function
    this makes it possible to test all possible combinations of the sanitizing methods, simply by adding the method name to get_all_attributes function in the data_parser.py

--- run.py
+++ run.py
@@ -2,9 +2,7 @@ from constants import TEST, TRAIN
 from data_builder import *
 from data_parser import Datasets
 from models.baseline_random import BaselineRandom
-from models.binary_naive_bayes import BinaryNaiveBayes
 from models.naive_bayes import NaiveBayes
-from models.add_k_naive_bayes import AddKNaiveBayes
 from models.logistic_regression import LogisticRegression
 from models.baseline_majority import BaselineMajority
 from benchmarker import Benchmarker
@@ -12,30 +10,24 @@ from benchmarker import Benchmarker
 
 if __name__ == "__main__":
 
-    # train_datasets = get_train_datasets()
-    # test_datasets = get_test_datasets()
+    variation_names = get_variations()
+    train_datasets = get_train_datasets()
+    test_datasets = get_test_datasets()
 
     # train_datasets = [Datasets(TRAIN)]
     # test_datasets = [Datasets(TEST)]
 
-    train_datasets = get_logistic_regression_train()
-    test_datasets = get_logistic_regression_test()
+    # train_datasets = get_logistic_regression_train()
+    # test_datasets = get_logistic_regression_test()
 
-    variation_names = get_logistic_regression_variations()
     to_be_benchmarked = []
 
     for i in range(len(train_datasets)):
-        # nb = NaiveBayes(train_datasets[i], variation_name=variation_names[i])
-        # to_be_benchmarked.append((nb, test_datasets[i]))
+        nb = NaiveBayes(train_datasets[i], variation_name=variation_names[i])
+        to_be_benchmarked.append((nb, test_datasets[i]))
 
-        # bnb = BinaryNaiveBayes(train_datasets[i], variation_name=variation_names[i])
-        # to_be_benchmarked.append((bnb, test_datasets[i]))
-
-        # aknb = AddKNaiveBayes(train_datasets[i], variation_name=variation_names[i])
-        # to_be_benchmarked.append((aknb, test_datasets[i]))
-
-        lr = LogisticRegression(train_datasets[i], variation_name=variation_names[i])
-        to_be_benchmarked.append((lr, test_datasets[i]))
+        aknb = NaiveBayes(train_datasets[i], variation_name=variation_names[i], k_factor=0.5)
+        to_be_benchmarked.append((aknb, test_datasets[0]))
 
     baseline_random = BaselineRandom(train_datasets[0])
     to_be_benchmarked.append((baseline_random, test_datasets[0]))
@@ -45,3 +37,6 @@ if __name__ == "__main__":
 
     benchmarker = Benchmarker(to_be_benchmarked)
     print(benchmarker.benchmark_models(20))
+
+
+
