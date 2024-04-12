6d9a74d8b4b8c7a49de46a219731da5c16cc8b02
Frederik Gantriis Møller <frmo24042@gmail.com>
2024-04-12 08:21:10 +0200

    fix: refactor run.py to update the range of j values in add_naive_bayes_models_with_k_factors function

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from models.logistic_regression import LogisticRegression
 from models.baseline_majority import BaselineMajority
 from benchmarker import Benchmarker
 
-def add_baseline_models(to_be_benchmarked, train_datasets, test_datasets):
+def add_baseline_models(to_be_benchmarked, train_datasets, test_datasets, variation_names):
     # Add BaselineRandom and BaselineMajority models to the list, both trained on the first training dataset
     to_be_benchmarked += [
         (BaselineRandom(train_datasets[0]), test_datasets[0]),
@@ -16,7 +16,7 @@ def add_baseline_models(to_be_benchmarked, train_datasets, test_datasets):
     ]
     return to_be_benchmarked
 
-def add_standard_naive_bayes_models(to_be_benchmarked, train_datasets, test_datasets):
+def add_standard_naive_bayes_models(to_be_benchmarked, train_datasets, test_datasets, variation_names):
     # For each training dataset, create a NaiveBayes model and pair it with the corresponding test dataset
     to_be_benchmarked += [
         (
@@ -27,7 +27,7 @@ def add_standard_naive_bayes_models(to_be_benchmarked, train_datasets, test_data
     ]
     return to_be_benchmarked
 
-def add_naive_bayes_models_with_k_factors(to_be_benchmarked, train_datasets, test_datasets):
+def add_naive_bayes_models_with_k_factors(to_be_benchmarked, train_datasets, test_datasets, variation_names):
     # Add more NaiveBayes models to the list, this time with varying k_factor values
     to_be_benchmarked += [
         (
@@ -37,11 +37,11 @@ def add_naive_bayes_models_with_k_factors(to_be_benchmarked, train_datasets, tes
             test_datasets[i],
         )
         for i in range(len(train_datasets))
-        for j in np.arange(0, 0.9, 0.1)
+        for j in np.arange(0.1, 0.9, 0.1)
     ]
     return to_be_benchmarked
 
-def add_logistic_regression_models(to_be_benchmarked, train_datasets, test_datasets):
+def add_logistic_regression_models(to_be_benchmarked, train_datasets, test_datasets, variation_names):
     # Add LogisticRegression models to the list
     to_be_benchmarked += [
         (LogisticRegression(train_datasets[i], variation_name=variation_names[i]), test_datasets[i])
@@ -58,24 +58,27 @@ if __name__ == "__main__":
     # Get all testing datasets
     test_datasets = get_test_datasets()
 
+    # train_datasets = [Datasets(TRAIN)]
+    # test_datasets = [Datasets(TEST)]
+    # variation_names = [""]
 
     # Initialize a list to hold all models to be benchmarked
     # For each training dataset, create a NaiveBayes model and pair it with the corresponding test dataset
-    to_be_benchmarked = add_standard_naive_bayes_models([], train_datasets, test_datasets)
+    to_be_benchmarked = add_standard_naive_bayes_models([], train_datasets, test_datasets, variation_names)
 
     # Add more NaiveBayes models to the list, this time with varying k_factor values
-    to_be_benchmarked += add_naive_bayes_models_with_k_factors(to_be_benchmarked, train_datasets, test_datasets)
+    to_be_benchmarked += add_naive_bayes_models_with_k_factors(to_be_benchmarked, train_datasets, test_datasets, variation_names)
 
     # Add LogisticRegression models to the list
-    to_be_benchmarked += add_logistic_regression_models(to_be_benchmarked, train_datasets, test_datasets)
+    # to_be_benchmarked += add_logistic_regression_models(to_be_benchmarked, train_datasets, test_datasets, variation_names)
 
     # Add BaselineRandom and BaselineMajority models to the list, both trained on the first training dataset
-    to_be_benchmarked += add_baseline_models(to_be_benchmarked, train_datasets, test_datasets)
+    to_be_benchmarked += add_baseline_models(to_be_benchmarked, train_datasets, test_datasets, variation_names)
 
     # Create a Benchmarker object with the list of models to be benchmarked
     benchmarker = Benchmarker(to_be_benchmarked, 10)
 
     # Print the results of benchmarking the models
-    print(benchmarker.benchmark_models())
+    benchmarker.benchmark_models()
 
-    benchmarker.create_all_charts()
+    # benchmarker.create_all_charts()
