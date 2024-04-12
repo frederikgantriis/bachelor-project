d3bf7b547184cc1769146ff434a4727d231d7cbc
Silas Bue Handgaard <silasbueh@hotmail.com>
2024-04-12 09:02:52 +0200

    correct svm usage and add features

--- models/svm.py
+++ models/svm.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from numpy.random import permutation
 from datasets import DatasetDict
+from sklearn.pipeline import FeatureUnion, make_pipeline
 from models.ml_algorithm import MLAlgorithm
 from constants import OFF, NOT
 import pandas as pd
@@ -17,85 +18,33 @@ class SVM(MLAlgorithm):
     def __init__(self, dataset: DatasetDict, variation_name=None) -> None:
         super().__init__(dataset, "svm", variation_name)
 
-        self.hateful_words: set = set(
-            pd.read_csv("./hurtlex_DA.tsv", sep="\t")["lemma"]
-        )
+        self.data = {}
+        off = [(x, "OFF") for x in self.dataset["OFF"]]
+        not_off = [(x, "NOT") for x in self.dataset["NOT"]]
 
-        self.data_length = len(self.dataset[OFF]) + len(self.dataset[NOT])
+        self.X = [x[0].text for x in off + not_off]
+        self.y = [x[1] for x in off + not_off]
+
+        self.df = pd.DataFrame(self.data)
+
         self.variation_name = ""
-        self.bias_term = 0
-        self.weights = [0, 0, 0]
-        self.svm_model = LinearSVC(dual="auto")
-
-        self.positive_words: set = set()
-        pos_words = open("data/sentiment-lexicons/positive_words_da.txt", "r")
-        while True:
-            word = pos_words.readline()
-            if not word:
-                break
-            self.positive_words.add(word[:-1])
-        pos_words.close()
-
-    def sigmoid(self, x):
-        z = numpy.exp(-x)
-        return float(1 / (1 + z))
-
-    def is_hateful(self, word: str) -> bool:
-        return word.lower() in self.hateful_words
-
-    def is_positive(self, word: str) -> bool:
-        return word.lower() in self.positive_words
+        self.svm_model = make_pipeline(FeatureUnion([
+            ('word_tfidf', TfidfVectorizer(analyzer='word', ngram_range=(1, 2))),
+            ('char_tfidf', TfidfVectorizer(analyzer='char', ngram_range=(2, 4)))
+        ]), SVC(kernel='linear', C=10))
 
     def train(self):
-        # Assuming the structure is ['sentence', 'label'], where label is "OFF" or "NOT"
-        for i in permutation(self.data_length):
-            if i < len(self.dataset[OFF]):
-                expected = 1
-                comment = self.dataset[OFF][i]
-            else:
-                expected = 0
-                comment = self.dataset[NOT][i - len(self.dataset[OFF])]
-
-            features = self.calculate_feature_amount(comment)
-
-        X = features.reshape(-1, 1)
-        y = self.classes
-
-        # Step 4: Train the SVM model
-        self.svm_model.fit(X, y)
+        self.svm_model.fit(self.X, self.y)
 
     def test(self, test_dataset_text):
         if self.svm_model is None:
             self.train()
 
         results = []
-        self.train()
 
         for test in test_dataset_text:
-            features = self.calculate_feature_amount(test)
+            y_pred = self.svm_model.predict([test.text])
 
-            result = self.svm_model.predict(
-                features.reshape(-1, 1))
-
-            results.append("OFF" if "OFF" in result else "NOT")
+            results.append("NOT" if "NOT" in y_pred else "OFF")
 
         return results
-
-    def calculate_feature_amount(self, comment):
-        """Assigns value to each feature based on comment then asignes their weight. Then normalises the output.
-
-        Args:
-            comment (list[str]): list of words in a specific comment
-
-        Returns:
-            list[int]: List of features amount ex. amount of hate words
-        """
-        features = numpy.array([0, 0])
-
-        for word in comment:
-            if self.is_hateful(word.text):
-                features[0] += 1
-            elif self.is_positive(word.text):
-                features[1] += 1
-
-        return features
--- run.py
+++ run.py
@@ -5,10 +5,15 @@ from models.svm import SVM
 
 
 if __name__ == "__main__":
-    svm = SVM(Datasets("train"))
-    svm.train()
-    result = svm.test(Datasets("test").to_list())
+    train_data = Datasets("train").remove_dots(
+    ).lemmatize().remove_stop_words()
 
-    bm = Benchmarker([(svm, Datasets("test"))], 10)
+    test_data = Datasets("test").remove_dots(
+    ).lemmatize().remove_stop_words()
+
+    svm = SVM(train_data)
+    svm.train()
+
+    bm = Benchmarker([(svm, test_data)], 10)
 
     print(bm.benchmark_models())
